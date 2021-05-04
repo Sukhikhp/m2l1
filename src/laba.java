46f2855a6f4b65bb58ee7e9461b3ba77a0e8cc63
Sukhikhp <Sukhikhp@internet.ru>
2021-05-04 19:09:15 +0300

    first load

---
+++ src/laba.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class laba {
+    public static void main(String[] args) {
+
+
+        //    System.out.println("введите число:");
+        //    int x = new Scanner(System.in).nextInt();
+        //    int s = new Scanner(System.in).nextInt();
+        //   System.out.println(x + s);
+        //   System.out.println(x - s);
+        //   System.out.println(x * s);
+        //  System.out.println(x / s);
+
+
+
+
+      /* System.out.println("введите число:");
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        System.out.println("введите число:");
+        int y = scanner.nextInt();
+        int a = x + y;
+        int b = x - y;
+        int c = x / y;
+        int v = x * y;
+        System.out.println(a + "\n" + b + "\n" + c + "\n" + v); */
+
+       /* System.out.println("введите число:");
+        int x = new Scanner(System.in).nextInt();
+        System.out.println(x*x);*/
+
+
+       /* System.out.println("введите число:");
+        int x = new Scanner(System.in).nextInt();
+        System.out.println("введите число:");
+        int y = new Scanner(System.in).nextInt();
+        System.out.println(x*y/100);*/
+
+
+
+
+
+    }
+}
+
+
